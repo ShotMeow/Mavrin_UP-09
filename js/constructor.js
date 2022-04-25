733a8ad03c5f7e4c37ad8d77c4ec46edfc6336f2
ShotMeow <shotmeow@gmail.com>
2022-04-25 11:47:46 +0300

    Fixed Constructor

--- js/constructor.js
+++ js/constructor.js
@@ -35,27 +35,49 @@ export const constructor = () => {
                     callapses[i].classList.add('in')
                 } else {
                     if (!button.getAttribute('href'))
-                        return
+                        if (!input.value) {
+                            input.focus()
+                            return
+                        } else
+                            return
                     callapses[i].classList.remove('in')
                 }
 
                 if (button.getAttribute('href') == '#collapseTwo') {
-                    if (checkbox.checked)
+                    if (checkbox.checked) {
                         result_1 = 10000
+                        document.querySelectorAll('#collapseTwo .title-text')[1].style.display = 'none'
+                        document.querySelectorAll('#collapseTwo .select-box')[2].style.display = 'none'
+                        document.querySelectorAll('#collapseTwo .select-box')[3].style.display = 'none'
+                    }
                     else
                         result_1 = 13000
                 }
 
                 if (button.getAttribute('href') == '#collapseThree') {
+                    if (!checkbox.checked) {
+                        if (secondDiameter.value.replace(/[^.\d]/g, '') == 1.4)
+                            diameter_2 = 1500
+                        else
+                            diameter_2 = 2000
+
+                        switch (secondCount.value.replace(/[^\d]/g, '')) {
+                            case '1':
+                                count_2 = diameter_2
+                                break
+                            case '2':
+                                count_2 = diameter_2 * 2
+                                break
+                            case '3':
+                                count_2 = diameter_2 * 3
+                                break
+                        }
+                    }
+
                     if (firstDiameter.value.replace(/[^.\d]/g, '') == 1.4)
                         diameter_1 = 1500
                     else
                         diameter_1 = 2000
-
-                    if (secondDiameter.value.replace(/[^.\d]/g, '') == 1.4)
-                        diameter_2 = 1500
-                    else
-                        diameter_2 = 2000
 
                     switch (firstCount.value.replace(/[^\d]/g, '')) {
                         case '1':
@@ -68,30 +90,18 @@ export const constructor = () => {
                             count_1 = diameter_1 * 3
                             break
                     }
-
-                    switch (secondCount.value.replace(/[^\d]/g, '')) {
-                        case '1':
-                            count_2 = diameter_2
-                            break
-                        case '2':
-                            count_2 = diameter_2 * 2
-                            break
-                        case '3':
-                            count_2 = diameter_2 * 3
-                            break
-                    }
-
-                    result_2 = count_1 + count_2
                 }
-
-                if (button.getAttribute('href') == '#collapseFour') {
-                    if (checkbox_2.checked)
-                        result_3 = 1000
-                }
-
-                result = result_1 + result_2 + result_3
+                result_2 = count_1 + count_2
             }
-            resultBlock.value = result
+
+            if (button.getAttribute('href') == '#collapseFour') {
+                if (checkbox_2.checked)
+                    result_3 = 1000
+            }
+
+            result = result_1 + result_2 + result_3
+
+            resultBlock.value = `${result} рублей`
         })
     })
 }
