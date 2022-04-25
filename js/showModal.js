f0c83f976d6a377f19f5f420714ec9d38f6728a5
ShotMeow <shotmeow@gmail.com>
2022-04-25 08:52:04 +0300

    Fixed Modal

--- js/showModal.js
+++ js/showModal.js
@@ -1,7 +1,9 @@
 export const showModal = () => {
-    const callButton = document.querySelector('.call-btn')
-    callButton.addEventListener('click', () => {
-        const modal = document.querySelector('.popup-call')
-        modal.style.display = 'block'
+    const callButtons = document.querySelectorAll('.call-btn')
+    callButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector('.popup-call')
+            modal.style.display = 'block'
+        })
     })
 }
