7a9aa535b3153880c153b4829d89ae27a0a774b6
ShotMeow <shotmeow@gmail.com>
2022-04-25 08:49:42 +0300

    Added Validation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { hideModal } from "./hideModal.js"
 import { questions } from "./questions.js"
 import { sendData } from "./sendData.js"
 import { showModal } from "./showModal.js"
+import { validation } from "./validation.js"
 
 hideModal()
 sendData()
 showModal()
 questions()
 constructor()
+validation()
